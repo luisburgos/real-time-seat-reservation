32fd0d1a654f35fae90146cd848e9104a92c540a
Jose Julio Heredia Barrera <josejulio@merostudios.com>
2016-03-13 21:16:58 -0600

    -Se implemento la generación del grid de los botones de los asientos

---
+++ src/ui/Buttons/SeatButton.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ui.Buttons;
+
+import java.awt.Color;
+import javax.swing.JButton;
+import ui.Util.ButtonStates;
+
+/**
+ *
+ * @author JoseJulio
+ */
+public class SeatButton extends JButton{
+    
+    private String currentState;
+    private int seatNumber;
+    
+    public SeatButton(int seatNumber){
+        this.seatNumber = seatNumber;
+        changeState(ButtonStates.FREE);
+    }
+    
+    public void changeState(String state){
+        currentState = state;
+        updateButton();
+    }
+    
+    private void updateButton(){
+        switch(currentState){
+            case ButtonStates.FREE: {
+                OnButtonFree();
+                break;
+            }
+            case ButtonStates.RESERVED: {
+                OnButtonReserved();
+                break;
+            }
+            case ButtonStates.SELECTED: {
+                OnButtonSelected();
+                break;
+            }
+            case ButtonStates.SOLD:{
+                OnButtonSold();
+                break;
+            }
+        }
+    }
+    
+    private void OnButtonSelected(){
+        System.out.println("Button was selected");
+        super.setBackground(Color.GRAY);
+    }
+    
+    private void OnButtonFree(){
+        System.out.println("Button was set free");
+        super.setBackground(Color.GREEN);
+    }
+    
+    private void OnButtonSold(){
+        System.out.println("Button was sold");
+        super.setBackground(Color.red);
+    }
+    
+    private void OnButtonReserved(){
+        System.out.println("Button was reserved");
+        super.setBackground(Color.BLUE);
+    }
+    
+}
