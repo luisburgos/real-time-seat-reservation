ca73f19e6983bdd4f09aad20948ff23233e79ebc
Maria Jose Patron Estrada <mariajose.patron@gmail.com>
2016-04-12 16:32:14 -0500

    Event handler fix

--- src/server/rooms/EventHandler.java
+++ src/server/rooms/EventHandler.java
@@ -8,6 +8,8 @@ package server.rooms;
 import client.remote.ClientRemote;
 import client.ui.buttons.ButtonStates;
 import java.rmi.RemoteException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Vector;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -15,15 +17,19 @@ import server.control.SeatsThreadPool;
 import server.control.tasks.SeatTask;
 import server.control.tasks.SelectedSeatTask;
 import server.data.SeatsRepository;
+import server.domain.Event;
 import server.domain.Seat;
 import server.remote.Server;
+import static server.remote.Server.clients;
+import server.remote.ServerRemote;
 import server.utils.ClientNotifier;
 
 /**
  *
  * @author User
  */
-public class EventHandler implements ServerRemote{
+
+public class EventHandler {
     
     private static final long serialVersionUID = 11L;
     private int event_id;
@@ -32,9 +38,9 @@ public class EventHandler implements ServerRemote{
     private ClientNotifier mClientNotifier;
     private SeatsThreadPool mPool;
     
-    public EventHandler() throws RemoteException {
+    public EventHandler(int event_id) throws RemoteException {
         super();
-        //this.event_id = event_id;
+        this.event_id = event_id;
         this.clients = new Vector<>();
         mPool = new SeatsThreadPool(50, 50);
         //UnicastRemoteObject.exportObject(this, 0);
@@ -59,6 +65,48 @@ public class EventHandler implements ServerRemote{
         } catch (Exception ex) {
             Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
         }
+    }
+    
+    private void notifyClients(int seatIndex, String newState){
+        
+        HashMap newStates = new HashMap<>();
+        newStates.put(seatIndex, newState);
+        
+        for(ClientRemote client : clients){
+            try {
+                client.updateSeatsState(newStates);
+            } catch (RemoteException ex) {
+                Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+
+    public void registerClient(ClientRemote client) throws RemoteException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void freeSeat(int seatNumber) throws RemoteException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void selectSeat(int seatNumber, int event_id) throws RemoteException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void reserveSeats(int[] seatNumbers) throws RemoteException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void cancelSeatsReservation() throws RemoteException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void buySeats(int[] seatNumbers) throws RemoteException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public Event getEvent(int eventID) throws RemoteException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
 }
